389fc46390c17fc4e6ee16b3dbee75bb8f87c1ba
邵翔 <370083084@qq.com>
2020-01-15 18:26:25 +0800

    +

--- src/main/java/com/esu/tim/TimApplication.java
+++ src/main/java/com/esu/tim/TimApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.esu.tim;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class TimApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(TimApplication.class, args);
 	}
 
 }
